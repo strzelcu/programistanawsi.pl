aa42af8bbf20c3bfabe1a961955322dccef21a2d
Tomasz Strzelecki <tomasz@strzelecki.opole.pl>
2025-10-01 21:43:26 +0200

    Theme initial commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tagi' },
+  { href: '/projects', title: 'Projekty' },
+  { href: '/about', title: 'O mnie' },
+]
+
+export default headerNavLinks
